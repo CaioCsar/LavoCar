26282312fa1a5014d3e40c58eb9a6ee3ed513a6e
Caio César <caiocsarbezerra95@gmail.com>
2023-05-22 18:53:36 -0300

    Inserçao do model Cliente

---
+++ LavoCar/Models/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LavoCar.Models
+{
+    public class Cliente
+    {
+
+        public long? ClienteID { get; set; }
+
+        [Required]
+        [Display(Name = "Nome")]
+        public string NomeCliente { get; set; }
+
+        [MaxLength(100)]
+        [Display(Name = "Endereço")]
+        public string EndCliente {get;set;}
+
+        [MaxLength(11)]
+        [Display(Name = "Telefone" )]
+        public string FoneCliente { get; set; }
+
+
+    }
+}

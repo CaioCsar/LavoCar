7ff627cac7a06f847f3879ec846a9db347bd9bb1
Henrique <85001190+HENRIQUE912@users.noreply.github.com>
2023-05-23 19:57:52 -0300

    Criação das model

---
+++ LavoCar/Models/Carro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LavoCar.Controllers
+{
+    public class Carro
+    {
+        public long? CarroID { get; set; }
+
+        public string Placa { get; set; }
+
+        public int Ano { get; set; }
+
+
+        public string Modelo { get; set; }
+
+        public string Marca { get; set; }
+
+    }
+}
---
+++ LavoCar/Models/Lavagem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LavoCar.Controllers
+{
+    public class Lavagem
+    {
+        public long? LavId { get; set; }
+        public DateTime DataLav { get; set; }
+
+        public int Valor { get; set; }
+    }
+}

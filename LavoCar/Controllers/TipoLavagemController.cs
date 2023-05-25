ae513d7f8bb3e93244f394a2721cca60a0e8984e
Caio <caiocsarbezerra95@gmail.com>
2023-05-24 21:25:28 -0300

    Fix de chaves em tipo de lavagem

--- LavoCar/Controllers/TipoLavagemController.cs
+++ LavoCar/Controllers/TipoLavagemController.cs
@@ -19,9 +19,10 @@ namespace LavoCar.Controllers {
 
         // GET: TipoLavagemController
 
-        public async Task<IActionResult> Index() {
-            return View(await _context.TipoLavagens.OrderBy(n=>n.DescTipoLav).ToListAsync());
-
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.TipoLavagens.OrderBy(n => n.DescTipoLav).ToListAsync());
+        }
         // GET: TipoLavagemController/Details/5
         public ActionResult Details(int id) {
             return View();

4675d7334664eb3b2a4c9bc40591f02a0f111e0c
Henrique <85001190+HENRIQUE912@users.noreply.github.com>
2023-05-30 20:25:28 -0300

    Feito alterações em lavagem

--- LavoCar/Controllers/LavagemController.cs
+++ LavoCar/Controllers/LavagemController.cs
@@ -33,7 +33,12 @@ namespace LavoCar.Controllers
             {
                 return NotFound();
             }
+            var tipolavagem = await _context.Lavagens.SingleOrDefaultAsync(m => m.LavID == id);
+            _context.TipoLavagens.Where(i => tipolavagem.TipoLavagemID == i.TipoLavID).Load();
+
             var lavagem = await _context.Lavagens.SingleOrDefaultAsync(m => m.LavID == id);
+            _context.Lavagens.Where(i => lavagem.LavID == i.LavID).Load();
+
             if (lavagem == null)
             {
                 return NotFound();
@@ -53,7 +58,7 @@ namespace LavoCar.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("DataLav, ValorLav")] Lavagem lavagem)
+        public async Task<IActionResult> Create([Bind("DataLav, TipoLavID")] Lavagem lavagem)
         {
             try
             {
@@ -93,10 +98,10 @@ namespace LavoCar.Controllers
         [ValidateAntiForgeryToken]
 
         //CONTINUAR FAZER AS AUTERÇÃO DO CONTROLE,DELETE
-        public async Task<ActionResult> Edit(long? id, [Bind("LavID, TipoLavagemID, DataLav")] Lavagem lavagem)
+        public async Task<ActionResult> Edit(long? id, [Bind("LavagemID, TipoLavagemID")] Lavagem lavagem)
         {
 
-            if (id != .CarroID)
+            if (id != lavagem.LavID)
             {
                 return NotFound();
             }
@@ -104,12 +109,12 @@ namespace LavoCar.Controllers
             {
                 try
                 {
-                    _context.Update(carro);
+                    _context.Update(lavagem);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!CarroExists(carro.CarroID))
+                    if (!LavagemExists(lavagem.LavID))
                     {
                         return NotFound();
                     }
@@ -118,31 +123,41 @@ namespace LavoCar.Controllers
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
+               
+            return RedirectToAction(nameof(Index));
             }
-            return View(carro);
+        return View(lavagem);
         }
+         private bool LavagemExists(long? id)
+         {
+        return _context.Lavagens.Any(e => e.LavID == id);
+            }
+
+    // GET: LavagemController/Delete/5
+    public  async Task<IActionResult> Delete(long? id)
+        {
+        if (id == null)
+        {
+            return NotFound();
+        }
+        var lavagem = await _context.Lavagens.SingleOrDefaultAsync(m => m.LavID == id);
+        _context.TipoLavagens.Where(i => lavagem.TipoLavagemID == i.TipoLavID).Load();
+        if (lavagem == null)
+        {
+            return NotFound();
+        }
+        return View(lavagem);
     }
 
-        // GET: LavagemController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
         // POST: LavagemController/Delete/5
-        [HttpPost]
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<IActionResult> DeleteConfirmed(long? id)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            var lavagem = await _context.Lavagens.SingleOrDefaultAsync(m => m.LavID == id);
+            _context.Lavagens.Remove(lavagem);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
